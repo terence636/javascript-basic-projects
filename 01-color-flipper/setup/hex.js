8fb2533823a2fba3bbc201e02a44dc327e8b3a83
Terence Chan <terence.kfc@gmail.com>
2021-05-23 19:56:00 +0800

    first commit

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,20 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const myButton = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+myButton.addEventListener("click",function () {
+    console.log("testing");
+    let hexValue = '#';
+    for(let i=0;i<6;i++) {
+        hexValue += hex[getRandomNumber()];
+        console.log(hexValue);
+    }
+
+    document.body.style.backgroundColor = hexValue;
+    color.textContent = hexValue;
+}
+)
+
+function getRandomNumber (){
+    return Math.floor(Math.random()*hex.length);
+}
